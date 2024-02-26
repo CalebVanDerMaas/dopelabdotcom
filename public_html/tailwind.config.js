d2214c3b2b06f5dc11d8486e61ac5450007d28b9
Caleb VanDerMaas <calebvandermaasc@gmail.com>
2024-02-25 21:55:25 -0500

    Restructure Dopelab Webstore for first public launch

---
+++ public_html/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+      colors: {
+        'dope-green': '#00cc99',
+        'dope-light-green': '#00E0A8',
+        'dope-crystal': '#ADFFEF',
+        'dope-grey': '#7B8482',
+        'dope-white': '#F3F7F6',
+        'dope-black': '#03110E',
+        'black': '#000000',
+        'white': '#FFFFFF'
+      },
+      extend: {
+        fontFamily: {
+          'dope': ['DopeFont', 'sans-serif']
+        }
+      },
+    },
+  plugins: [],
+}
+
